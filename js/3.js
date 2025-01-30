602e4759632f70ab47c37d53e55860f2eee579e8
ShivamVerma95 <161635657+ShivamVerma95@users.noreply.github.com>
2025-01-30 11:56:40 +0000

    data types

--- js/3.js
+++ js/3.js
@@ -1,1 +1,26 @@
+/* data types are of two types they are primitve and derived data types
+
+Primitive data types includes(7 types):string,number,boolean,null,symbol,undefined,bigInt
+derived includes:array,object and function
+*/
+ let Name="shivam"; //string
+ let id=1223; //number
+ let isvalid=false; //boolean
+ let outsideTemp=null; //null
+ let symbol=122;  //symbol
+ let name;  //undefined
+ let number=1234567890n;   //bigInt -> js automatically takes it as bigint
+
+ let studentName=["shivam","shreyansh","james"];
+ let myobejct=
+ {
+    sname:"shivam",
+    branch:"cse"
+ }
+ let myfunc=function(){
+    console.log("hello world!");
+    
+ }
+myfunc()
+
 
